c7c27e54db7c75ac4b2c328fec02142987bd68d1
Fadi Asharma <fadi.asharma@michaelhill.com.au>
2021-08-11 13:35:49 +1000

    First commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
+// Controll everything. It runs when we run this application
 import Vue from 'vue'
+// import app.vue root component to our main.js file
 import App from './App.vue'
+import navs from './components/navication.vue'
+
+Vue.component('navication', navs);
 
 Vue.config.productionTip = false
 
 new Vue({
+  // reender our app.vue root component to our Vue instance
   render: h => h(App),
 }).$mount('#app')
