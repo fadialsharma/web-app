d568803c5efafb182f1575079a7424f8fce70097
Fadi Asharma <fadi.asharma@michaelhill.com.au>
2021-08-18 00:23:12 +1000

    Using props in header and footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,20 @@
 <template>
     <footer>
         <div class="footer-container">
+            <div class="col-3">
+                <h3>{{navTitle}}</h3>
+                <ul>
+                    <li v-for="(nav, index) in navication" v-bind:key="index">{{nav}}</li>
+                </ul>
+            </div>
+            <div class="col-3">
+                <h3>{{subscribeTitle}}</h3>
+            </div>
+            <div class="col-3">
+                <h3>{{mapTitle}}</h3>
+            </div>
+        </div>
+        <div class="copyrights">
             <p>{{copyrights}}</p>
         </div>
     </footer>
@@ -9,9 +23,14 @@
 <script>
 // import
 export default {
+    props: ['navication'],
     data() {
         return {
-            copyrights: 'All Rights Reserved. Designed by Fadi Asharma.'
+            copyrights: 'All Rights Reserved. Designed by Fadi Asharma.',
+            navTitle: 'Navication',
+            subscribeTitle: 'Subscribe',
+            mapTitle: 'Map',
+
         }
     }
 }
@@ -19,13 +38,29 @@ export default {
 
 <style scoped>
 footer {
-    background: black;
+    background: #34495E;
     color: white;
 }
 .footer-container {
+    display: flex;
     max-width: 1280px;
     margin: 0 auto;
     padding: 15px;
+}
+.footer-container .col-3 {
+    width: 33.333%;
+}
+.footer-container .col-3 ul {
+    text-align: left;
+    list-style: none;
+    padding: 0px 15px;
+}
+.footer-container .col-3 ul li{
+    margin: 15px 0px;
+    text-decoration: underline;
+}
+footer .copyrights {
     text-align: center;
+    padding: 0px 0px 32px 0px;
 }
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,52 @@
 <template>
     <header>
-        <ul>
-            <li v-for="(nav, index) in navication" v-bind:key="index">{{ nav }}</li>
-        </ul>
+        <div class="header-container">
+            <div class="col-3">
+                <div class="logo">{{logoTitle}}</div>
+            </div>
+            <div class="col-3">
+                <ul>
+                    <li v-for="(nav, index) in navication" v-bind:key="index">{{ nav }}</li>
+                </ul>
+            </div>
+            <div class="col-3">
+                <div class="search">{{SearchTitle}}</div>
+            </div>
+        </div>
     </header>
 </template>
 
 <script>
 export default {
+    props: ['navication'],
     data() {
         return {
-            navication: ['Home', 'About','Services','Contact']
+            logoTitle: 'Logo',
+            SearchTitle: 'Search'
         }
     }
 }
 </script>
 
-
 <style scoped>
 header {
-    background: lightblue;
+    background: #3FB983;
+}
+.header-container {
+    display: flex;
+    max-width: 1280px;
+    padding: 15px;
+    margin: 0 auto;
+}
+.header-container .col-3 {
+    width: 33.333%;
+}
+.header-container .col-3 .search{
+    text-align: right;
 }
 ul {
     display: flex;
     justify-content: space-between;
-    padding: 15px;
-    max-width: 1280px;
     margin: 0 auto;
 }
 li {
